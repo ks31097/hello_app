4cf81275ec1950c31551b544db394f449e4c26a9
Kostya Sigarov <ksigaryov@gmail.com>
2022-07-06 15:55:51 +0300

    Update ApplicationController file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   def hello
-    render plain: "Hello, world!"
+    render html: "Hello, world!" # render html: "Hello, world!" <=> render plain: "Hello, world!"
   end
 end
